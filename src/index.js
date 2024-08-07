f7122186c5b680c9a1cbce3c1f91b5c771756bd9
Yaraslau Haurylavets <mirlednev@tutamail.com>
2024-08-07 15:53:58 +0300

    brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(a => a[0] + a[1]);
+  let checkStr = "";
+  while(str.length!=checkStr.length){
+    checkStr = str;
+    for(let i of bracketsConfig){
+      str = str.replaceAll(i, "");
+    }
+  }
+  return str.length==0;
 }
